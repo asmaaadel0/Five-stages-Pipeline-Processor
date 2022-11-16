14123e3ea7c1283952519f3f5c21845339fb4ea6
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-11-16 20:18:38 +0200

    update integration file

--- data_stack_memory.v
+++ data_stack_memory.v
@@ -1,6 +1,7 @@
 module data_stack_memory #(parameter data_width=16,address_width=32,num_of_register=11)
 
-(input wire clk ,write_enable,read_enable,input wire [(data_width-1):0] write_data ,input wire [(address_width-1):0] address,output reg  [(data_width-1):0] read_data ) ;
+//(input wire clk ,write_enable,read_enable,input wire [(data_width-1):0] write_data ,input wire [(address_width-1):0] address,output reg  [(data_width-1):0] read_data ) ;
+(input wire clk ,write_enable,read_enable,input wire [(data_width*2-1):0] write_data ,input wire [(address_width-1):0] address,output reg  [(data_width*2-1):0] read_data ) ;
 
 reg [(data_width-1):0] array_reg [2**num_of_register-1:0];
 integer i;
@@ -8,7 +9,7 @@ always @(posedge clk )
  begin
 
 if(write_enable)
-    array_reg[address] =write_data ;
+    {array_reg[address], array_reg[address+1]} =write_data ;
 
  end
 
@@ -16,7 +17,8 @@ always @(negedge clk)
  begin
 
  if(read_enable)
- read_data =array_reg[address];
+ //for store and load we care about array_reg[address] which are the most 16bit
+ read_data ={array_reg[address], array_reg[address+1]};
 else
   read_data='bz;
 
--- mux_generic.v
+++ mux_generic.v
@@ -9,3 +9,15 @@ assign out=(sel==0)? i0:i1; //just a line?
 
 
 endmodule
+
+
+module mux_generic_2bit_selector #(parameter width=24)
+        (   input [width-1:0]i0,i1,i2,i3,
+            input [1:0] sel,
+            output reg [width-1:0]out
+            );
+assign out=(sel==00)? i0:
+           (sel==01)? i1:
+		   (sel==10)? i2: i3;
+
+endmodule
